61d16f0e81b70be9e36a4dfeeaaece0a854c28d9
Iain Caldwell <iain.s.caldwell@gmail.com>
2017-10-17 17:14:58 +0100

    Bug fixes

--- hello.go
+++ hello.go
@@ -7,7 +7,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("Serving request ", req.Header)
 		w.Write([]byte("Hello, gophers!"))
 	})
-	http.ListenAndServe("8000", nil)
+	http.ListenAndServe(":8000", nil)
 }
